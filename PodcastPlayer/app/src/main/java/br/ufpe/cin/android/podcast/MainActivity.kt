02d84418e91fef940454481c121dfb9558537e4f
Rodrigo Carneiro <rodrigo.carneiro@inlocomedia.com>
2019-09-15 15:15:17 -0300

    feat(MainActivity.kt): Create recycler view

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -2,15 +2,32 @@ package br.ufpe.cin.android.podcast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import br.ufpe.cin.android.podcast.adapters.PodcastAdapter
 import br.ufpe.cin.android.podcast.tasks.PodcastParserTask
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: PodcastAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        PodcastParserTask().execute(PODCAST_URL)
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = PodcastAdapter(emptyList(), this)
+
+        recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply {
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+        recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+
+        PodcastParserTask(viewAdapter).execute(PODCAST_URL)
     }
 
     companion object {
