f8449e24aa540f585630eae5a98b6fa621a67249
Rodrigo Carneiro <rodrigo.carneiro@inlocomedia.com>
2019-09-15 17:02:07 -0300

    feat(PodcastParserTask.kt): Save episodes on database and get them if there is no internet

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/tasks/PodcastParserTask.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/tasks/PodcastParserTask.kt
@@ -1,21 +1,35 @@
 package br.ufpe.cin.android.podcast.tasks
 
 import android.os.AsyncTask
+
+import android.util.Log
 import br.ufpe.cin.android.podcast.ItemFeed
 import br.ufpe.cin.android.podcast.Parser
 import br.ufpe.cin.android.podcast.adapters.PodcastAdapter
 import br.ufpe.cin.android.podcast.dal.DownloadXml
+import br.ufpe.cin.android.podcast.dal.ItemFeedDB
+import java.io.IOException
 
-class PodcastParserTask(private val adapter: PodcastAdapter) :
+class PodcastParserTask(private val adapter: PodcastAdapter, private val db: ItemFeedDB) :
     AsyncTask<String, Int, List<ItemFeed>>() {
-    override fun doInBackground(vararg parameters: String?): List<ItemFeed> {
-        val xml = DownloadXml().download(parameters[0]!!)
+    override fun doInBackground(vararg parameters: String?): List<ItemFeed>? {
+        return try {
+            val xml = DownloadXml().download(parameters[0]!!)
+            db.itemFeedDao().insert(Parser.parse(xml))
+            Parser.parse(xml)
+        } catch (e: IOException) {
+            Log.e(CONNECTION_TAG, "Could not get list of episodes from the xml")
 
-        return Parser.parse(xml)
+            db.itemFeedDao().selectAll()
+        }
     }
 
     // Update list of episodes after downloading it
-    override fun onPostExecute(result: List<ItemFeed>?) {
-        adapter.updateEpisodeList(result!!)
+    override fun onPostExecute(result: List<ItemFeed>) {
+        adapter.updateEpisodeList(result)
+    }
+
+    companion object {
+        const val CONNECTION_TAG = "connectionError"
     }
 }
