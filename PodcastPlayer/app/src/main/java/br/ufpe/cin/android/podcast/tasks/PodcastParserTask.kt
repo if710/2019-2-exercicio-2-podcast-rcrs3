397192f7d581f820455204ee86fd20f62d0c750f
Rodrigo Carneiro <rodrigo.carneiro@inlocomedia.com>
2019-09-15 15:14:14 -0300

    feat(PodcastParserTask.kt): Update the list of episodes when finish downloading it

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/tasks/PodcastParserTask.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/tasks/PodcastParserTask.kt
@@ -3,12 +3,19 @@ package br.ufpe.cin.android.podcast.tasks
 import android.os.AsyncTask
 import br.ufpe.cin.android.podcast.ItemFeed
 import br.ufpe.cin.android.podcast.Parser
+import br.ufpe.cin.android.podcast.adapters.PodcastAdapter
 import br.ufpe.cin.android.podcast.dal.DownloadXml
 
-class PodcastParserTask : AsyncTask<String, Int, List<ItemFeed>>() {
+class PodcastParserTask(private val adapter: PodcastAdapter) :
+    AsyncTask<String, Int, List<ItemFeed>>() {
     override fun doInBackground(vararg parameters: String?): List<ItemFeed> {
         val xml = DownloadXml().download(parameters[0]!!)
 
         return Parser.parse(xml)
+    }
+
+    // Update list of episodes after downloading it
+    override fun onPostExecute(result: List<ItemFeed>?) {
+        adapter.updateEpisodeList(result!!)
     }
 }
