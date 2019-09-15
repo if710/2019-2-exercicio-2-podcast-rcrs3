709b176a13ddc6de2b589821bb8d984aec42be90
Rodrigo Carneiro <rodrigo.carneiro@inlocomedia.com>
2019-09-15 17:01:27 -0300

    feat(DownloadXml.kt): Throws IOException

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/dal/DownloadXml.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/dal/DownloadXml.kt
@@ -1,8 +1,10 @@
 package br.ufpe.cin.android.podcast.dal
 
+import java.io.IOException
 import java.net.URL
 
 class DownloadXml {
+    @Throws(IOException::class)
     fun download(path: String): String {
         val url = URL(path)
 
