32198c7cb4d569b8ffb8360a02a2d2491ff7d74a
Rodrigo Carneiro <rodrigo.carneiro@inlocomedia.com>
2019-09-15 12:53:19 -0300

    feat(MainActivity.kt): Call the PodcatParserTask

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -2,11 +2,18 @@ package br.ufpe.cin.android.podcast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.ufpe.cin.android.podcast.tasks.PodcastParserTask
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        PodcastParserTask().execute(PODCAST_URL)
+    }
+
+    companion object {
+        const val PODCAST_URL = "https://s3-us-west-1.amazonaws.com/podcasts.thepolyglotdeveloper.com/podcast.xml"
     }
 }
