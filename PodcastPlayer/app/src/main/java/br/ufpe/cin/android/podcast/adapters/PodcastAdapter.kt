2c52239b405940bd6867a088b761741d9a9f40d9
Rodrigo Carneiro <rodrigo.carneiro@inloco.com.br>
2019-09-19 09:28:15 -0300

    feat(PodcastAdapter.kt): Create intent to call EpisodeDetailActivity

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/adapters/PodcastAdapter.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/adapters/PodcastAdapter.kt
@@ -1,19 +1,23 @@
 package br.ufpe.cin.android.podcast.adapters
 
-import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import br.ufpe.cin.android.podcast.EpisodeDetailActivity
 import br.ufpe.cin.android.podcast.ItemFeed
+import br.ufpe.cin.android.podcast.MainActivity
 import br.ufpe.cin.android.podcast.R
 import kotlinx.android.synthetic.main.itemlista.view.*
 
-class PodcastAdapter (private var episodes: List<ItemFeed>, private val context: Context):
+class PodcastAdapter (private var episodes: List<ItemFeed>, private val application: MainActivity):
     RecyclerView.Adapter<PodcastAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val episodeView =
-            LayoutInflater.from(context).inflate(R.layout.itemlista, parent, false)
+            LayoutInflater
+                .from(application.applicationContext)
+                .inflate(R.layout.itemlista, parent, false)
 
         return ViewHolder(episodeView)
     }
@@ -25,6 +29,20 @@ class PodcastAdapter (private var episodes: List<ItemFeed>, private val context:
 
         holder.title?.text = episode.title
         holder.pubDate?.text = episode.pubDate
+
+        setOnClickListener(holder, episode)
+    }
+
+    fun setOnClickListener(holder: ViewHolder, episode: ItemFeed) {
+        holder.itemView.setOnClickListener {
+            val intent = Intent(application.applicationContext, EpisodeDetailActivity::class.java)
+
+            intent.putExtra(EpisodeDetailActivity.EPISODE_TITLE, episode.title)
+            intent.putExtra(EpisodeDetailActivity.EPISODE_LINK, episode.link)
+            intent.putExtra(EpisodeDetailActivity.EPISODE_DESCRIPTION, episode.description)
+
+            application.startActivity(intent)
+        }
     }
 
     // Pass a new list of episodes and notify
